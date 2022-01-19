ea078943993cca543ae662cc04cdfef6b6d6e702
yan xu <yan.xu@thoughtworks.com>
2022-01-19 17:38:28 +0800

    update shell script

--- scripts/shell/2-data-tree-export.sh
+++ scripts/shell/2-data-tree-export.sh
@@ -12,4 +12,9 @@ AND CALENDAR_YEAR(CloseDate) = $CURRENT_YEAR"
 echo "Running command: sfdx force:data:tree:export. Pulling from ${ORG_ALIAS} 
 org. Records and Plan output to : ${FILE_PATH}"
 
-sfdx force:data:tree:export -q "$(echo $QUERY)" -d ${FILE_PATH} -p --json -u ${ORG_ALIAS}
+# LIMIT: export up to 2,000 records per cmd.
+sfdx force:data:tree:export -q "$(echo $QUERY)" -d ${FILE_PATH} -p -u ${ORG_ALIAS}
+
+# LIMIT: import up to 200 records per cmd.
+# sfdx force:data:tree:import -p data/Plan1.json -u ${ORG_ALIAS}
+# sfdx force:data:tree:import -p data/Plan2.json -u ${ORG_ALIAS}
